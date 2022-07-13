fb488711f62f0fa68a102a3532c93d2844d79573
david98pp <luisdavidpineda@gmail.com>
2022-07-13 18:30:36 -0500

    Add Tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,74 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Control de stickers'),
-
-        ///TODO hacer tabs
-        //  bottom: TabBar(tabs: tabs),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            // padding: EdgeInsets.zero,
+            children: [
+              /* const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),*/
+              ListTile(
+                title: const Text('Estadísticas'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text('Control de stickers'),
+          centerTitle: true,
+          elevation: 2,
+          bottom: const TabBar(
+            unselectedLabelColor: Color(0xFF8EC552),
+            indicatorColor: Color(0xFF8EC552),
+            labelColor: Color(0xFF8EC552),
+            labelStyle: TextStyle(fontSize: 11.5),
+            tabs: [
+              Tab(text: "Todos"),
+              Tab(text: "Faltantes"),
+              Tab(text: "Repetidos"),
+              Tab(text: "Equipos"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Center(
+              //TODO ListView to GridView
+              child: ListView.builder(
+                itemCount: 256,
+                itemBuilder: (context, index) {
+                  return Container();
+                },
+              ),
+            ),
+            const Center(
+              child: Text('Faltantes'),
+            ),
+            const Center(
+              child: Text('Repetidos'),
+            ),
+            const Center(
+              child: Text('Equipos'),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
